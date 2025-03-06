ecef276b9c205abb41df58d660a879481fc574e5
finnhet <149392001+finnhet@users.noreply.github.com>
2025-03-06 12:33:08 +0100

    usercontroller uit web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\VacancyController;
 
 Route::get('/', function () {
