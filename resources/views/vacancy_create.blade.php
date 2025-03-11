49714750800c97860ab4486ce49f6a86d954d223
finnhet <149392001+finnhet@users.noreply.github.com>
2025-03-11 17:33:04 +0100

    header toegevoged aan vacancy create

--- resources/views/vacancy_create.blade.php
+++ resources/views/vacancy_create.blade.php
@@ -9,34 +9,36 @@
 </head>
 
 <body class="bg-light">
-  <div class="container mt-5">
-    <div class="card p-4 shadow-lg rounded mx-auto" style="max-width: 600px;">
-      <h3 class="text-center mb-4">Vacature Aanmaken</h3>
-      <form class="row g-3" action="{{ url('/vacancy_create') }}" method="POST">
-        @csrf
-        <div class="col-12">
-          <label for="inputTitle" class="form-label">Titel</label>
-          <input type="text" class="form-control" id="inputTitle" name="title" placeholder="Titel">
-        </div>
-        <div class="col-12">
-          <label for="inputIntroduction" class="form-label">Introductie</label>
-          <input type="text" class="form-control" id="inputIntroduction" name="introduction" placeholder="Introductie">
-        </div>
-        <div class="col-12">
-          <label for="inputDescription" class="form-label">Beschrijving</label>
-          <textarea class="form-control" id="inputDescription" name="description" rows="3" placeholder="Beschrijving"></textarea>
-        </div>
-        <div class="col-12">
-          <label for="inputLocation" class="form-label">Locatie</label>
-          <input type="text" class="form-control" id="inputLocation" name="location" placeholder="Locatie">
-        </div>
+  <x-app-layout>
+    <div class="container mt-5">
+      <div class="card p-4 shadow-lg rounded mx-auto" style="max-width: 600px;">
+        <h3 class="text-center mb-4">Vacature Aanmaken</h3>
+        <form class="row g-3" action="{{ url('/vacancy_create') }}" method="POST">
+          @csrf
+          <div class="col-12">
+            <label for="inputTitle" class="form-label">Titel</label>
+            <input type="text" class="form-control" id="inputTitle" name="title" placeholder="Titel" required>
+          </div>
+          <div class="col-12">
+            <label for="inputIntroduction" class="form-label">Introductie</label>
+            <input type="text" class="form-control" id="inputIntroduction" name="introduction" placeholder="Introductie" required>
+          </div>
+          <div class="col-12">
+            <label for="inputDescription" class="form-label">Beschrijving</label>
+            <textarea class="form-control" id="inputDescription" name="description" rows="3" placeholder="Beschrijving" required></textarea>
+          </div>
+          <div class="col-12">
+            <label for="inputLocation" class="form-label">Locatie</label>
+            <input type="text" class="form-control" id="inputLocation" name="location" placeholder="Locatie" required>
+          </div>
 
-        <div class="col-12 text-center">
-          <button type="submit" class="btn btn-primary w-100">Vacature Aanmaken</button>
-        </div>
-        </>
+          <div class="col-12 text-center">
+            <button type="submit" class="btn btn-primary w-100">Vacature Aanmaken</button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
+  </x-app-layout>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
