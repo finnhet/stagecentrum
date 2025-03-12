7bf1b6f265e3abb5476e227ea912fa74d571544a
bramus123 <251972@edu.rocfriesepoort.nl>
2025-03-12 09:09:44 +0100

    Vacancy-Edit Front-End

--- resources/views/vacancy-edit.blade.php
+++ resources/views/vacancy-edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Edit Vacancy') }}
+            {{ __('Bewerk Vacature') }}
         </h2>
     </x-slot>
 
