b0cd8c0959be304a648e088331d38c9da9bbb4b7
finnhet <149392001+finnhet@users.noreply.github.com>
2025-03-17 19:15:52 +0100

    beschrijving kunnen toevoegen en aanpassen

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,6 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
@@ -21,9 +22,28 @@
 
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
+                    <form method="POST" action="{{ route('profile.update') }}">
+                        @csrf
+                        @method('PATCH')
+
+                        <div class="mb-4">
+                            <label for="description" class="block font-medium text-sm text-gray-700">beschrijving</label>
+                            <textarea name="description" id="description" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" rows="4">{{ old('description', auth()->user()->description) }}</textarea>
+                        </div>
+
+                        <div class="flex justify-end">
+                            <x-primary-button>{{ __('opslaan') }}</x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
+
         </div>
     </div>
 </x-app-layout>
