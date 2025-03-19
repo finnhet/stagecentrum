6066f45b83b1266bd34e6e283c16abde37add64a
finnhet <149392001+finnhet@users.noreply.github.com>
2025-03-19 16:30:59 +0100

    styling op searchbar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,58 +7,82 @@
     
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
+<style>
+    <style>
+    .navbar {
+        padding: 0.8rem 1rem; 
+        box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1); 
+    }
+
+    .navbar a {
+        font-weight: 500;
+    }
+
+    .form-inline .form-control {
+        border-radius: 20px;
+        padding: 0.5rem 1rem;
+        max-width: 250px;
+    }
+
+    .dropdown-menu {
+        border-radius: 8px;
+        border: none;
+        box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+    }
+</style>
+
+</style>
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
             <div class="container">
-                <div class="shrink-0 flex items-center">
                 <a href="{{ url('/') }}" style="color: black; text-decoration: none; font-size: 1.25rem; font-weight: bold;">
                     Stagecentrum
                 </a>
-            </div>
 
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" 
+                <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarNav" 
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <form action="{{ route('vacancies.search') }}" method="GET" class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" name="filter" placeholder="Search for a filter..." aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ml-auto">
-                        @if (Route::has('login'))
-                            @auth
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ url('/dashboard') }}">Dashboard</a>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        {{ Auth::user()->name }}
-                                    </a>
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                                        <a class="dropdown-item" href="{{ route('profile.edit') }}">profiel</a>
-                                        <div class="dropdown-divider"></div>
-                                        <form method="POST" action="{{ route('logout') }}">
-                                            @csrf
-                                            <button type="submit" class="dropdown-item">Log uit</button>
-                                        </form>
-                                    </div>
-                                </li>
-                            @else
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">Log in</a>
-                                </li>
-                                @if (Route::has('register'))
+                    <div class="d-flex flex-column flex-lg-row w-100 justify-content-end align-items-center">
+                        <form action="{{ route('vacancies.search') }}" method="GET" class="form-inline my-2 my-lg-0 w-100 w-lg-auto">
+                            <input class="form-control mr-sm-2" type="search" name="filter" placeholder="Zoek op een filter..." aria-label="Search">
+                        </form>
+
+                        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                            @if (Route::has('login'))
+                                @auth
                                     <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('register') }}">Registreren</a>
+                                        <a class="nav-link" href="{{ url('/dashboard') }}">Dashboard</a>
                                     </li>
-                                @endif
-                            @endauth
-                        @endif
-                    </ul>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            {{ Auth::user()->name }}
+                                        </a>
+                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                                            <a class="dropdown-item" href="{{ route('profile.edit') }}">profiel</a>
+                                            <div class="dropdown-divider"></div>
+                                            <form method="POST" action="{{ route('logout') }}">
+                                                @csrf
+                                                <button type="submit" class="dropdown-item">Log uit</button>
+                                            </form>
+                                        </div>
+                                    </li>
+                                @else
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('login') }}">Log in</a>
+                                    </li>
+                                    @if (Route::has('register'))
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="{{ route('register') }}">Registreren</a>
+                                        </li>
+                                    @endif
+                                @endauth
+                            @endif
+                        </ul>
+                    </div>
                 </div>
             </div>
         </nav>
