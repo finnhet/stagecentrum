29af27b4fd27278418dd7df16512069c3e4525be
finnhet <finnh@laptopje.>
2025-03-20 14:32:01 +0100

    searchbar op rechter kant van header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,28 +7,6 @@
     
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<style>
-    .navbar {
-        padding: 0.8rem 1rem; 
-        box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1); 
-    }
-
-    .navbar a {
-        font-weight: 500;
-    }
-
-    .form-inline .form-control {
-        border-radius: 20px;
-        padding: 0.5rem 1rem;
-        max-width: 250px;
-    }
-
-    .dropdown-menu {
-        border-radius: 8px;
-        border: none;
-        box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
-    }
-</style>
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
@@ -43,43 +21,43 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <div class="d-flex flex-column flex-lg-row w-100 justify-content-end align-items-center">
-                        <form action="{{ route('vacancies.search') }}" method="GET" class="form-inline my-2 my-lg-0 w-100 w-lg-auto">
-                            <input class="form-control mr-sm-2" type="search" name="filter" placeholder="Zoek op een filter..." aria-label="Search">
-                        </form>
-
-                        <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-                            @if (Route::has('login'))
-                                @auth
+                    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                        <li class="nav-item d-flex align-items-center">
+                            <form action="{{ route('vacancies.search') }}" method="GET" class="form-inline my-2 my-lg-0">
+                                <input class="form-control mr-sm-2" type="search" name="filter" placeholder="Zoek op een filter..." aria-label="Search">
+                            </form>
+                        </li>
+                        
+                        @if (Route::has('login'))
+                            @auth
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('/dashboard') }}">Dashboard</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        {{ Auth::user()->name }}
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                                        <a class="dropdown-item" href="{{ route('profile.edit') }}">profiel</a>
+                                        <div class="dropdown-divider"></div>
+                                        <form method="POST" action="{{ route('logout') }}">
+                                            @csrf
+                                            <button type="submit" class="dropdown-item">Log uit</button>
+                                        </form>
+                                    </div>
+                                </li>
+                            @else
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">Log in</a>
+                                </li>
+                                @if (Route::has('register'))
                                     <li class="nav-item">
-                                        <a class="nav-link" href="{{ url('/dashboard') }}">Dashboard</a>
+                                        <a class="nav-link" href="{{ route('register') }}">Registreren</a>
                                     </li>
-                                    <li class="nav-item dropdown">
-                                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            {{ Auth::user()->name }}
-                                        </a>
-                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                                            <a class="dropdown-item" href="{{ route('profile.edit') }}">profiel</a>
-                                            <div class="dropdown-divider"></div>
-                                            <form method="POST" action="{{ route('logout') }}">
-                                                @csrf
-                                                <button type="submit" class="dropdown-item">Log uit</button>
-                                            </form>
-                                        </div>
-                                    </li>
-                                @else
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="{{ route('login') }}">Log in</a>
-                                    </li>
-                                    @if (Route::has('register'))
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="{{ route('register') }}">Registreren</a>
-                                        </li>
-                                    @endif
-                                @endauth
-                            @endif
-                        </ul>
-                    </div>
+                                @endif
+                            @endauth
+                        @endif
+                    </ul>
                 </div>
             </div>
         </nav>
